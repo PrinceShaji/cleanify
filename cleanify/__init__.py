4ec66d6871c0f04f963d265882254e600cf11d5d
Prince Shaji <22243569+PrinceShaji@users.noreply.github.com>
2019-05-06 15:45:12 +0530

    Fixed the iteration problem while searching through the bad words file

--- cleanify/__init__.py
+++ cleanify/__init__.py
@@ -1,33 +1,45 @@
+#usr/bin/env python3
 # Cleanify 0.1 beta
 # Program to create clean, profanity free UUIDs.
 
 
 class uuid:
-	#To search for a match in the db (txt file).
-	def filter(string, file):
+	#To search for a match in the bad words db (txt file).
+	def filter_uuid(string, file='bad_words.txt'):
 		import re
 		string = string.lower()
 		with open(file, 'r') as fin:
+			counter = 0
 			for lines in fin:
 				if re.findall(lines, string):
-					return True
+					counter += 1
+					break
+			#Checking the counter to see if the for loop encountered any bad words.
+			if counter == 0:
+				return False
+			else:
+				return True
 
 
-	def write(string, file):
+	def write_uuid(string, file):
 		with open(file, 'a') as fout:
 			fout.write(string)
 			
-	def random(algo, length):
+	def random_uuid(algo, length):
 		import string, random
 		algorithms = {base64:string.ascii_letters+string.digits+"-_"}
 		
 		#To check if the user has selected the correct algoritm
 		if algo not in algorithms:
+			print("yes")
 			return False
-			break
 		else:
-			continue
+			random_uuid = "".join(random.SystemRandom().choices(algorithms[algo], k=length))
+			return random_uuid
 
-		random_uuid = "".join(random.SystemRandom().choices(algorithms[algo], k=length))
 
-		return random_uuid
+
+if __name__ == '__main__':
+	algo = input("algo:")
+	length = int(input("length"))
+	uuid.random_uuid()
